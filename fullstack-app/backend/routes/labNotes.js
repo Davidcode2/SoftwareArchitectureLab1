e043e0ec943e7bdf834f0638a6649cd02ae8b31a
Jakob Lingel <jlingel68@gmail.com>
2023-03-17 21:30:16 +0100

    Add routes to backend

---
+++ fullstack-app/backend/routes/labNotes.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const config = require('../config.json');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log(config.labNotes);
+  fs.readFile('/home/jakob/OneDrive/HochschuleAA/bachelor-3/Software_Architecture/lab1/frontend_notes.md', 'utf8', (err, data) => {
+    if (err) {
+      res.status(404).send('Not Found');
+    }
+    res.send(data);
+  },);
+});
+
+module.exports = router;
